4751b852223cf72e59780f0920881d8b053712b0
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2019-09-03 21:02:25 +0800

    feat: Add functionality to identify firstCard and secondCard clicked

--- scripts.js
+++ scripts.js
@@ -1,7 +1,22 @@
 const cards = document.querySelectorAll('.memory-card');
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard() {
     this.classList.toggle('flip');
+
+    if (!hasFlippedCard) {
+        // first click
+        hasFlippedCard = true;
+        firstCard = this;
+    } else {
+        // second click
+        hasFlippedCard = false;
+        secondCard = this;
+
+        // do cards match?
+    }
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
